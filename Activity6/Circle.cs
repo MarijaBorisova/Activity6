1f32acd22b82854176c8c1ab35c81ee3ef7f864c
MarijaNet <marija.borisovanet@gmail.com>
2021-12-05 18:22:50 +0200

    Add project files.

---
+++ Activity6/Circle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace CSharp.Activity.Polymorphism
+{
+    public class Circle : Shape
+    {
+        public double Radius { get; protected set; }
+
+        public Circle(double Radius) : base()
+        {
+            this.Radius = Radius;
+        }
+
+        public Circle()
+        {
+        }
+
+        public override void CalculateArea()
+        {
+            Area = Math.PI * Radius * Radius;
+
+        }
+
+
+        public override void Print()
+        {
+        }
+    }
+
+}
+
---
+++ Program/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using CSharp.Activity.Polymorphism;
+
+
+namespace CSharp.Activity.Polymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circ = new Circle(5);
+            Rectangle rect = new Rectangle(2, 3);
+        }
+    }
+}
